930086100c29433939241ee2a8bcf1a04a2333f1
pretoniano <vicgabriel.bahia@gmail.com>
2023-02-25 02:49:58 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,28 @@
+$(document).ready(() => {
+
+    $('#btn_iniciar').click(() => {
+        $('form').slideDown();
+        $('#btn_iniciar').slideUp();
+        $('#btn_encerrar').slideDown();
+    })
+    $('#btn_encerrar').click(() => {
+        $('form').slideUp();
+        $('#btn_iniciar').slideDown();
+        $('#btn_encerrar').slideUp();
+        $('li').remove();
+    })
+
+    $('form').on('submit', (e) => {
+        e.preventDefault();
+        const conteudo = $('#tarefa').val();
+        const novaTarefa = $('<li style = "display: none; color: black; text-decoration: black; cursor: pointer"></li>');
+        $(`<p>${conteudo}</p>`).appendTo(novaTarefa);
+        $(novaTarefa).appendTo('ul');
+        $('li').slideDown();
+        
+        $(novaTarefa).click(() => {
+            $(novaTarefa).css("text-decoration", "line-through");
+        })
+    })
+
+})
